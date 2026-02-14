2e9afe45756d97a2b9efea4d78fc7bb3cf01bf27
Ivan-Dubinov <dubinov.ivan@bk.ru>
2026-02-14 12:49:33 +0300

    feat: внедрить автоматическое обновление статуса epic на основе подзадач

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,65 @@
 public class Main {
     public static void main(String[] args) {
-        TaskManager taskManager = new TaskManager();
+        TaskManager manager = new TaskManager();
 
-        Epic epic = new Epic(taskManager.generateId(), "Переезд", "Организовать переезд",
+        Epic epic = new Epic(manager.generateId(), "Переезд", "Организовать переезд",
                 TaskStatus.NEW);
-        taskManager.addTask(epic);
+        manager.addTask(epic);
 
         Subtask packBoxes = new Subtask(
-                taskManager.generateId(),
+                manager.generateId(),
                 "Собрать коробки",
-                "Упоковать вещи в коробки",
+                "Упоковать вещи",
                 TaskStatus.NEW,
                 epic.getId()
         );
 
-        Task task1 = new Task(taskManager.generateId(), "Покушать", "Приготовить завтрак", TaskStatus.NEW);
-
-        Task task2 = new Task(taskManager.generateId(), "Погулять", "Пойти погулять на улице", TaskStatus.NEW);
-
-        taskManager.addTask(packBoxes);
-
+        manager.addTask(packBoxes);
         epic.addSubtaskId(packBoxes.getId());
-        taskManager.addTask(task1);
-        System.out.println(taskManager.getTaskById(3));
-        taskManager.updateTask(task2, task1.getId());
-        System.out.println(taskManager.getTaskById(4));
+
+        Subtask sayGoodBye = new Subtask(
+                manager.generateId(),
+                "Попрощаться",
+                "Сказать соседям пока",
+                TaskStatus.NEW,
+                epic.getId()
+        );
+        manager.addTask(sayGoodBye);
+        epic.addSubtaskId(sayGoodBye.getId());
+
+        System.out.println("=== Проверка обновление Epic ===");
+        System.out.println("===После создания===");
+        System.out.println(epic);
+
+        packBoxes.setStatus(TaskStatus.DONE);
+        manager.updateSubTask(packBoxes);
+
+        System.out.println("\n=== После обновления одной подзадачи на DONE ===");
+        System.out.println(manager.getTaskById(epic.getId()));
+
+        sayGoodBye.setStatus(TaskStatus.DONE);
+        manager.updateSubTask(sayGoodBye);
+
+        System.out.println("\n=== После обновлнеия второй подзадачи на DONE ===");
+        System.out.println(manager.getTaskById(epic.getId()));
+
+        System.out.println("\n=== Проверка обновления задачи ===");
+
+        Task task1 = new Task(manager.generateId(), "Покушать", "Приготовить завтрак", TaskStatus.NEW);
+
+        Task task2 = new Task(manager.generateId(), "Погулять", "Пойти погулять на улице", TaskStatus.NEW);
+        manager.addTask(task1);
+        System.out.println(manager.getTaskById(task1.getId()));
+        manager.updateTask(task2, task1.getId());
+        System.out.println(manager.getTaskById(task2.getId()));
+
+        System.out.println("\n=== Прочие проверки === ");
+
         System.out.println(epic);
         System.out.println(packBoxes);
-        System.out.println(taskManager.getTaskById(epic.getId()));
-        System.out.println(taskManager.getTasks());
-        taskManager.removeAllTasks();
-        System.out.println(taskManager.getTasks());
+        System.out.println(manager.getTaskById(epic.getId()));
+        System.out.println(manager.getTasks());
+        manager.removeAllTasks();
+        System.out.println(manager.getTasks());
     }
 }
--- src/TaskManager.java
+++ src/TaskManager.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class TaskManager {
@@ -13,6 +15,21 @@ public class TaskManager {
         return tasks.get(taskId);
     }
 
+    public List<Subtask> getSubtasksByEpicId(int epicId) {
+        List<Subtask> result = new ArrayList<>();
+        for (Task task : tasks.values()) {
+            if (task instanceof Subtask) {
+                Subtask subtask = (Subtask) task;
+                if (subtask.getEpicId() == epicId) {
+                    result.add(subtask);
+                }
+            }
+        }
+        return result;
+    }
+
+
+
     public void addTask(Task task){
         tasks.put(task.getId(), task);
     }
@@ -26,11 +43,51 @@ public class TaskManager {
         return nextId++;
     }
 
+
     public void removeAllTasks(){
         tasks.clear();
     }
 
     public void removeTaskById(int taskId){
         tasks.remove(taskId);
+    }
+
+
+    public void updateSubTask(Subtask updatedSubtask){
+        tasks.put(updatedSubtask.getId(), updatedSubtask);
+
+        int epicId = updatedSubtask.getEpicId();
+        Task epicTask = tasks.get(epicId);
+
+        if (epicTask instanceof Epic) {
+            Epic epic = (Epic) epicTask;
+
+            updateEpicStatus(epic);
+        }
+    }
+
+    private void updateEpicStatus(Epic epic) {
+        List<Subtask> subtasks = getSubtasksByEpicId(epic.getId());
+
+        if (subtasks.isEmpty()) {
+            epic.setStatus(TaskStatus.NEW);
+            return;
+        }
+
+        boolean allNew = true;
+        boolean allDone = true;
+
+        for (Subtask subtask : subtasks) {
+            if (subtask.getStatus() != TaskStatus.NEW) allNew = false;
+            if (subtask.getStatus() != TaskStatus.DONE) allDone = false;
+        }
+
+        if (allNew) {
+            epic.setStatus(TaskStatus.NEW);
+        } else if (allDone) {
+            epic.setStatus(TaskStatus.DONE);
+        } else {
+            epic.setStatus(TaskStatus.IN_PROGRESS);
+        }
     }
 }

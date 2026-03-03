730992c09c9ac10e1e070d12433435718b36fbc2
Ivan-Dubinov <dubinov.ivan@bk.ru>
2026-03-03 09:50:53 +0300

    refactor: implement core structure for duplicate-free history using custom LinkedList + HashMap - Added HistoryManager interface with add/remove/getHistory - Created internal Node class for doubly-linked list - Added head/tail pointers and nodeMap (id → Node) - Implemented linkLast() and removeNode() for O(1) list operations

--- src/InMemoryHistoryManager.java
+++ src/InMemoryHistoryManager.java
@@ -1,7 +1,14 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class InMemoryHistoryManager implements HistoryManager{
+    private final Map<Integer, Node> nodeMap = new HashMap<>();
+    private Node head;
+    private Node tail;
+    private int maxSize = 10;
+
     private final List<Task> history = new ArrayList<>();
 
     @Override
@@ -16,8 +23,44 @@ public class InMemoryHistoryManager implements HistoryManager{
     }
 
     @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
     public List<Task> getHistory() {
         return new ArrayList<>(history);
     }
-}
 
+    private void linkLast(Node node){
+        if (tail == null){
+            head = tail = node;
+        } else {
+            tail.next = node;
+            node.prev = tail;
+            tail = node;
+        }
+    }
+    private void removeNode(Node node) {
+        if (node.prev != null) {
+            node.prev.next = node.next;
+        } else {
+            head = node.next;
+        }
+        if (node.next != null){
+            node.next.prev = node.prev;
+        } else {
+            tail = node.prev;
+        }
+    }
+
+    private static class Node {
+        Task task;
+        Node prev;
+        Node next;
+
+        Node(Task task){
+            this.task = task;
+        }
+    }
+}

7f1c5032414e3b69b6b3c9fef52ee45e34ff48c4
Ivan-Dubinov <dubinov.ivan@bk.ru>
2026-03-16 10:38:30 +0300

    feat: implements CSV serialization for Task, Subtask, Epic - Added static toString(Task) method to convert task to CSV line - Added static fromString(String) to restore task from CSV line - Supports all task types: TASK, SUBTASK, EPIC

--- src/Task.java
+++ src/Task.java
@@ -43,6 +43,45 @@ public class Task {
         this.description = description;
     }
 
+    public static String toString(Task task) {
+        String type;
+        int epicId = 0;
+
+        if (task instanceof Subtask) {
+            type = "SUBTASK";
+            epicId = ((Subtask) task).getEpicId();
+        } else if (task instanceof Epic) {
+            type = "EPIC";
+        } else {
+            type = "TASK";
+        }
+
+        return String.format("%d,%s,%s,%s,%s,%d", task.id, type, task.name, task.status, task.description, epicId);
+    }
+
+
+    public static Task fromString(String value) {
+        String[] partsOfTask = value.split(",");
+
+        if (partsOfTask.length != 6) {
+            throw new IllegalArgumentException("Неверный формат строки задачи");
+        }
+
+        int id = Integer.parseInt(partsOfTask[0]);
+        String type = partsOfTask[1];
+        String name = partsOfTask[2];
+        TaskStatus status = TaskStatus.valueOf(partsOfTask[3]);
+        String description = partsOfTask[4];
+        int epicId = Integer.parseInt(partsOfTask[5]);
+
+        return switch (type) {
+            case "SUBTASK" -> new Subtask(id, name, description, status, epicId);
+            case "EPIC" -> new Epic(id, name, description, status);
+            case "TASK" -> new Task(id, name, description, status);
+            default -> throw new IllegalArgumentException("Неизвестный тип задачи: " + type);
+        };
+    }
+
     @Override
     public String toString(){
         return String.format("Task:{id=%d, name='%s', status=%s, description='%s'}", id, name, status, description);

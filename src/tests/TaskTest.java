fc284aeda33def0023a78143795ad486a2c8c4dd
Ivan-Dubinov <dubinov.ivan@bk.ru>
2026-04-12 23:42:28 +0300

    feat: начал писать тесты для Task

---
+++ src/tests/TaskTest.java
@@ -0,0 +1,32 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void getStatus() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void fromString() {
+    }
+}

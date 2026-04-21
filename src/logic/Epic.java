4484785176d1fc814ebc37ddbf189296d03f6080
Ivan-Dubinov <dubinov.ivan@bk.ru>
2026-04-21 23:09:02 +0300

    feat: добавить регулирование endTime в Epic (getEndTime и setEndTime)

--- src/logic/Epic.java
+++ src/logic/Epic.java
@@ -1,18 +1,34 @@
 package logic;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Epic extends Task{
-    private List<Integer> subTaskIds;
+    private final List<Integer> subTaskIds;
+    private LocalDateTime endTime;
 
     public Epic(int id, String name, String description, TaskStatus status){
         super(id, name, description, status);
         subTaskIds = new ArrayList<>();
     }
+    public Epic(int id, String name, String description, TaskStatus status, Duration duration, LocalDateTime localDateTime){
+        super(id, name, description, status, duration, localDateTime);
+        subTaskIds = new ArrayList<>();
+    }
 
     public List<Integer> getSubTaskIds() {
         return new ArrayList<>(subTaskIds);
+    }
+
+    @Override
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    protected void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
     }
 
     public void addSubtaskId(int subTaskId) {
